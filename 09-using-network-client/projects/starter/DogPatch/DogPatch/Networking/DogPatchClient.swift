5c138466169dca206fae7d05fde7ac2f29167f4d
leed <lee.d.881010@gmail.com>
2022-05-23 23:46:57 +0900

    🟢 test_shared_setsBaseURL

--- 09-using-network-client/projects/starter/DogPatch/DogPatch/Networking/DogPatchClient.swift
+++ 09-using-network-client/projects/starter/DogPatch/DogPatch/Networking/DogPatchClient.swift
@@ -36,7 +36,7 @@ class DogPatchClient {
   
   // 더미 값이 있는 static shared property 정의
   static let shared = DogPatchClient(
-    baseURL: URL(string:"https://example.com")!,
+    baseURL: URL(string:"https://dogpatchserver.herokuapp.com/api/v1/")!,
     session: URLSession(configuration: .default),
     responseQueue: nil)
   
